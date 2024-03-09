ec73aff3eef177664018b95953ca1e711bc23ab6
e.ghalibaf <erfanwmb@gmail.com>
2024-03-09 16:54:01 +0330

    update notification alert service

--- src/NotificationAlertService.php
+++ src/NotificationAlertService.php
@@ -6,6 +6,10 @@ use Illuminate\Support\Facades\Session;
 
 class NotificationAlertService
 {
+    public function __construct()
+    {
+        Session::flush('notification-alert');
+    }
 
     /**
      * @param string|null $title
@@ -15,12 +19,25 @@ class NotificationAlertService
      */
     public static function info(string $title = null, string $message = null, string $mode = null): void
     {
-        Session::flash('notification-alert', [
-            'type' => 'info',
-            'mode' => $mode ?? config('notification-alert.notification.default'),
-            'title' => $title,
-            'message' => $message,
-        ]);
+        if (Session::has('notification-alert')) {
+            $old_flush = Session::get('notification-alert');
+            $old_flush[] = [
+                'type' => 'info',
+                'mode' => $mode ?? config('notification-alert.notification.default'),
+                'title' => $title,
+                'message' => $message,
+            ];
+            Session::flash('notification-alert', $old_flush);
+        } else {
+            Session::flash('notification-alert', [
+                [
+                    'type' => 'info',
+                    'mode' => $mode ?? config('notification-alert.notification.default'),
+                    'title' => $title,
+                    'message' => $message,
+                ]
+            ]);
+        }
     }
 
     /**
@@ -31,12 +48,25 @@ class NotificationAlertService
      */
     public static function warning(string $title = null, string $message = null, string $mode = null): void
     {
-        Session::flash('notification-alert', [
-            'type' => 'warning',
-            'mode' => $mode ?? config('notification-alert.notification.default'),
-            'title' => $title,
-            'message' => $message,
-        ]);
+        if (Session::has('notification-alert')) {
+            $old_flush = Session::get('notification-alert');
+            $old_flush[] = [
+                'type' => 'warning',
+                'mode' => $mode ?? config('notification-alert.notification.default'),
+                'title' => $title,
+                'message' => $message,
+            ];
+            Session::flash('notification-alert', $old_flush);
+        } else {
+            Session::flash('notification-alert', [
+                [
+                    'type' => 'warning',
+                    'mode' => $mode ?? config('notification-alert.notification.default'),
+                    'title' => $title,
+                    'message' => $message,
+                ]
+            ]);
+        }
     }
 
 
@@ -48,12 +78,25 @@ class NotificationAlertService
      */
     public static function error(string $title = null, string $message = null, string $mode = null): void
     {
-        Session::flash('notification-alert', [
-            'type' => 'danger',
-            'mode' => $mode ?? config('notification-alert.notification.default'),
-            'title' => $title,
-            'message' => $message,
-        ]);
+        if (Session::has('notification-alert')) {
+            $old_flush = Session::get('notification-alert');
+            $old_flush[] = [
+                'type' => 'error',
+                'mode' => $mode ?? config('notification-alert.notification.default'),
+                'title' => $title,
+                'message' => $message,
+            ];
+            Session::flash('notification-alert', $old_flush);
+        } else {
+            Session::flash('notification-alert', [
+                [
+                    'type' => 'error',
+                    'mode' => $mode ?? config('notification-alert.notification.default'),
+                    'title' => $title,
+                    'message' => $message,
+                ]
+            ]);
+        }
     }
 
     /**
@@ -64,11 +107,24 @@ class NotificationAlertService
      */
     public static function success(string $title = null, string $message = null, string $mode = null): void
     {
-        Session::flash('notification-alert', [
-            'type' => 'success',
-            'mode' => $mode ?? config('notification-alert.notification.default'),
-            'title' => $title,
-            'message' => $message,
-        ]);
+        if (Session::has('notification-alert')) {
+            $old_flush = Session::get('notification-alert');
+            $old_flush[] = [
+                'type' => 'success',
+                'mode' => $mode ?? config('notification-alert.notification.default'),
+                'title' => $title,
+                'message' => $message,
+            ];
+            Session::flash('notification-alert', $old_flush);
+        } else {
+            Session::flash('notification-alert', [
+                [
+                    'type' => 'success',
+                    'mode' => $mode ?? config('notification-alert.notification.default'),
+                    'title' => $title,
+                    'message' => $message,
+                ]
+            ]);
+        }
     }
 }
